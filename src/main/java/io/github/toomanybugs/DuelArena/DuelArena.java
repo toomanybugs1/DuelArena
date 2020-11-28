6abba4b25bcd612feeba492b0e3c450127a580e8
Trey Shaw <treyshaw81@gmail.com>
2020-11-28 12:47:35 -0600

    initial commit:

---
+++ src/main/java/io/github/toomanybugs/DuelArena/DuelArena.java
@@ -0,0 +1,15 @@
+package io.github.toomanybugs.DuelArena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DuelArena extends JavaPlugin {
+	@Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
